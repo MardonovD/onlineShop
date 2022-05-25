750f0ce0e7e44eb7c3378345e864b9e900d6317e
Dostonbek <mardonovdoston69@gmail.com>
2022-05-25 15:29:24 +0300

    main page

---
+++ src/components/Section.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/Section.scss";
+
+const Section = () => {
+  return (
+    <section className="main-section">
+      <div className="content">
+        <h1>Mening Online Do'konimga Xush Kelibsiz...</h1>
+        <button>Discover</button>
+      </div>
+      <div className="bottom-text">
+        <p>10% off for products </p>
+      </div>
+    </section>
+  );
+};
+
+export default Section;
